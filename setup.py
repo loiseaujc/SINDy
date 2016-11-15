ec38beeb10d0ac6d3bf54aaf8cbc63df408a7673
mq <mquade@uni-potsdam.de>
2016-11-15 23:27:21 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
-import sparse_identification as sp
+
+VERSION = "0.0.1"
+
+def read_requirements():
+    with open("requirements.txt", "r") as f:
+        content = f.readlines()
+    return [s.split("\n")[0] for s in content]
+
 
 setup(name='sparse_identification',
-      version=sp.__version__,
+      version=VERSION,
       packages=find_packages(),
       author="Jean-Christophe Loiseau",
       author_email='loiseau.jc@gmail.com',
       description='Python library designed for sparse identification of nonlinear dynamical system',
       classifiers=["Programming Language :: Python",
-                   "Topic :: Fluid dynamics"], requires=['numpy', 'scipy', 'cvxopt']
+                   "Topic :: Fluid dynamics"],
+      requires=read_requirements()
       )
